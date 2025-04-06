91c72e9e6f23cbb7053174467ced2fb21a843c76
Pushkar Kurhekar <pshkrh@gmail.com>
2025-04-06 19:16:49 -0400

    Trying new db connection method

--- model_pipeline/scripts/db_connection.py
+++ model_pipeline/scripts/db_connection.py
@@ -1,3 +1,4 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from config import DB_NAME, USER, PASSWORD, HOST, PORT
@@ -12,4 +13,26 @@ DB_CONFIG = {
 
 
 def get_db_connection():
-    return psycopg2.connect(**DB_CONFIG, cursor_factory=RealDictCursor)
+    # For Cloud Run connecting to Cloud SQL
+    if os.environ.get("K_SERVICE"):
+        # Cloud SQL connection using Unix socket
+        db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+        instance_connection_name = os.environ.get("INSTANCE_CONNECTION_NAME")
+
+        return psycopg2.connect(
+            dbname=DB_NAME,
+            user=USER,
+            password=PASSWORD,
+            host=f"{db_socket_dir}/{instance_connection_name}",
+            cursor_factory=RealDictCursor,
+        )
+    else:
+        # Local development connection using TCP
+        return psycopg2.connect(
+            dbname=DB_NAME,
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            cursor_factory=RealDictCursor,
+        )
